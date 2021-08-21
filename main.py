5a647a9cdb3743dc6a1c0e2a108b7546d8df6aaf
SecondThundeR <awayfromgalaxy@gmail.com>
2021-08-22 01:36:20 +0300

    main: update
    - Added "Processing x song(s)" message
    - Finalized output (Moved printing unavailable tracks to the end)

--- main.py
+++ main.py
@@ -12,18 +12,20 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="ID",
 def check_loved_tracks_unavailability():
     offset_counter = 0
     unavailable_tracks = 0
+    unavailable_tracks_dict = {}
     print('Checking your "Loved Tracks"...')
     start_time = time.perf_counter()
     tracks = sp.current_user_saved_tracks(limit=50)
     while tracks['items']:
         for i, item in enumerate(tracks['items']):
             track = sp.track(item['track']['id'], market='from_token')
-            track_name = f"\'{track['artists'][0]['name']} - {track['name']}\'"
-            track_pos = f"[{(i + 1) + offset_counter}]"
             if track['is_playable'] is False:
                 unavailable_tracks += 1
-                print(f"{track_pos} Track {track_name} is unavailable in your country")
+                track_name = f"\'{track['artists'][0]['name']} - {track['name']}\'"
+                track_pos = f"[{(i + 1) + offset_counter}]"
+                unavailable_tracks_dict[track_pos] = track_name
         offset_counter += len(tracks['items'])
+        print(f'Processed {offset_counter} song(s)...', end='\r')
         tracks = sp.current_user_saved_tracks(limit=50, offset=offset_counter)
     stop_time = time.perf_counter()
     final_time = stop_time - start_time
@@ -31,7 +33,10 @@ def check_loved_tracks_unavailability():
         print(f"All ({offset_counter}) tracks are available for listening!")
     else:
         print(f"{unavailable_tracks} track(s) out of {offset_counter} track(s) are unavilable!")
-    print(f'Your "Loved Tracks" checked for {final_time} seconds')
+        print("\nHere are all list of unavailable songs:")
+        for pos, name in unavailable_tracks_dict.items():
+            print(f"{pos} Track {name} is unavailable in your country")
+    print(f'\nYour "Loved Tracks" checked for {final_time} seconds')
 
 
 if __name__ == '__main__':
