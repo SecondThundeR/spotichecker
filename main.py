52c0f8271f1b0fd23f58b449a29dbae0fddecb9e
Владислав <awayfromgalaxy@gmail.com>
2021-08-23 17:28:48 +0300

    main: perfomance update
    Current code was slightly rewritten to use `tracks` method instead of `track` method. This change significantly boosted perfomance and reduced track processing time by about ~12 times
    ===
    Tests (Tracks: 4565 / CPU: i5-10210U / Low Power Mode: Enabled):
    Before changes: 480.0917277 seconds
    After changes: 37.5342798 seconds

--- main.py
+++ main.py
@@ -11,17 +11,20 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="ID",
 
 def check_loved_tracks_unavailability():
     offset_counter = 0
-    unavailable_tracks = 0
+    unavailable_tracks_counter = 0
     unavailable_tracks_dict = {}
-    print('Checking your "Loved Tracks"...')
+    print('Processing your "Loved Tracks"...')
     start_time = time.perf_counter()
     tracks = sp.current_user_saved_tracks(limit=50)
     while tracks['items']:
-        for i, item in enumerate(tracks['items']):
-            track = sp.track(item['track']['id'], market='from_token')
-            if track['is_playable'] is False:
-                unavailable_tracks += 1
-                track_name = f"\'{track['artists'][0]['name']} - {track['name']}\'"
+        tracks_id_list = []
+        for item in tracks['items']:
+            tracks_id_list.append(item['track']['id'])
+        checked_tracks = sp.tracks(tracks=tracks_id_list, market='from_token')
+        for i, item in enumerate(checked_tracks['tracks']):
+            if item['is_playable'] is False:
+                unavailable_tracks_counter += 1
+                track_name = f"\'{item['artists'][0]['name']} - {item['name']}\'"
                 track_pos = f"[{(i + 1) + offset_counter}]"
                 unavailable_tracks_dict[track_pos] = track_name
         offset_counter += len(tracks['items'])
@@ -29,10 +32,10 @@ def check_loved_tracks_unavailability():
         tracks = sp.current_user_saved_tracks(limit=50, offset=offset_counter)
     stop_time = time.perf_counter()
     final_time = stop_time - start_time
-    if unavailable_tracks == 0:
+    if unavailable_tracks_counter == 0:
         print(f"All ({offset_counter}) tracks are available for listening!")
     else:
-        print(f"{unavailable_tracks} track(s) out of {offset_counter} track(s) are unavilable!")
+        print(f"{unavailable_tracks_counter} track(s) out of {offset_counter} track(s) are unavilable!")
         print("\nHere are all list of unavailable songs:")
         for pos, name in unavailable_tracks_dict.items():
             print(f"{pos} Track {name} is unavailable in your country")
