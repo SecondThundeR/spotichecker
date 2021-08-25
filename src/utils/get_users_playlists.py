89b3c0fe969bebbfc2f038e5f637ebc25a76f593
Владислав <awayfromgalaxy@gmail.com>
2021-08-25 17:17:50 +0300

    get_users_playlists: misc style fix

--- src/utils/get_users_playlists.py
+++ src/utils/get_users_playlists.py
@@ -1,11 +1,11 @@
 def get_users_playlists(sp):
     # TODO: Save playlist IDs to dict
     user_data = sp.me()
-    playlists = sp.user_playlists(user_data['display_name'])
+    playlists = sp.user_playlists(user_data["display_name"])
     while playlists:
-        for i, playlist in enumerate(playlists['items']):
+        for i, playlist in enumerate(playlists["items"]):
             print(f"{i + 1}. {playlist['name']}")
-        if playlists['next']:
+        if playlists["next"]:
             playlists = sp.next(playlists)
         else:
             playlists = None
