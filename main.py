2d9d9abbc60415f885da3eaa88abba5d468cc528
SecondThundeR <awayfromgalaxy@gmail.com>
2021-09-09 18:02:24 +0300

    main: update code
    - Moved out scopes to global variable
    - Added more scopes for checking user playlists
    - Added entry point for checking user's playlists
    - Reordered some elements in menu
    - Slightly changed visual styling of menu

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ from src.utils.check_playlist_tracks import check_playlist_tracks
 from src.utils.credentials_manager import initial_configuration
 from src.utils.get_users_playlists import get_users_playlists
 
+SCOPES = "user-library-read, playlist-read-private, playlist-read-collaborative"
+
 
 def credentials_checker():
     """Check if credentials file.
@@ -47,7 +49,7 @@ def login_to_spotify(credentials):
         client_id=credentials["CLIENT_ID"],
         client_secret=credentials["CLIENT_SECRET"],
         redirect_uri="http://localhost:8080",
-        scope="user-library-read",
+        scope=SCOPES
     ))
     return sp
 
@@ -59,30 +61,30 @@ def main_menu(sp):
     input from user to run certain check functions.
 
     Args:
-        sp (spotipy.oauth2.SpotifyOAuth): Spotify OAuth object.
+        sp (spotipy.oauth2.SpotifyOAuth): Spotify OAuth object
     """
-    print("Welcome to SpotiChecker!\n")
+    print("=== Welcome to SpotiChecker! ===\n")
     while True:
+        print("========= Menu Options =========")
         print("Choose option to continue:")
         print('1. Check your "Loved Tracks"')
-        print("2. Check playlist by ID")
-        print("3. Check playlist on your account")
+        print("2. Check your created playlists")
+        print("3. Check playlist by ID")
         print("0. Exit")
         try:
             option = int(input("> "))
         except ValueError:
-            print("You entered something wrong. Try again\n")
+            print("[Error] You entered something wrong. Try again\n")
             continue
         if option == 1:
             check_loved_tracks(sp)
         elif option == 2:
+            get_users_playlists(sp)
+        elif option == 3:
             playlist_id = input("\nEnter playlist ID: ")
             check_playlist_tracks(sp, playlist_id)
-        elif option == 3:
-            print("\n[Info] This feature currently not implemented!\n")
-            # get_users_playlists(sp)
         elif option == 0:
-            print("See you soon!")
+            print("=== See you soon! ===")
             sys.exit()
 
 
